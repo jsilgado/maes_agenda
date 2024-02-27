d174df524b7332c70342a9a1f821f9236de6f8f5
Jesús Silgado Carrero <jesus.silgado@babelgroup.com>
2024-02-27 17:29:59 +0100

    Actividad 1 Hello World

---
+++ src/main/java/com/maes/agenda/controller/Bienvenida.java
@@ -0,0 +1,15 @@
+package com.maes.agenda.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class Bienvenida {
+
+	@GetMapping
+	public String hello() {
+		return "Hello World";
+	}
+}
